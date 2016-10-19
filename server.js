1748dc0224f605dbb1d9e37921b6de7abd260275
Robert <rlancer@arachno.com>
2016-10-19 00:02:24 -0400

    change feed and index in place

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import http from 'http';
+import rethinkdbdash from 'rethinkdbdash';
 import randomNameGenerator from './randomNameGenerator';
+import algoliasearch from 'algoliasearch';
+import config from './config';
 
-const r = require("rethinkdbdash")({db: 'algolia'});
+// RethinkDB client, using rethinkdbdash for async await
+const r = rethinkdbdash({db: 'algolia'});
 
-// Setup database and table
+// Algolia client
+const algolia = algoliasearch(config.applicationId, config.adminApiKey);
+
+// Create `users` index
+const userIndex = algolia.initIndex('users');
+// Sets the attributes to index
+userIndex.setSettings({attributesToIndex: ['first', 'last']}).then();
+
 (async()=> {
-    await r.dbList().contains('algolia')
-        .do(exists =>r.branch(exists, true, r.dbCreate('algolia')));
+    // Create database `algolia` if it doesn't exist
+    await r.dbList().contains('algolia').branch(true, r.dbCreate('algolia'));
 
-    await r.tableList().contains('users')
-        .do(exists =>r.branch(exists, true, r.tableCreate('users')));
+    // Create table `users` if it doesn't exist
+    await r.tableList().contains('users').branch(true, r.tableCreate('users'));
+
+    // Listen to changes on tabale `users`
+    (await r.table('users').changes()).each(async(err, item)=> {
+        const {new_val, old_val} = item;
+        if (new_val) {
+            // Add changed values to the `users` index
+            const resp = await userIndex.addObject({objectID: new_val.id, ...new_val});
+            console.log('Added', new_val, '\nTo the `users` index', resp);
+        }
+    });
 })();
+
 
 const app = express();
 app.use(express.static('front/build'));
@@ -31,6 +52,4 @@ app.get('/populate', async(req, res) =>
 
 const port = process.env.PORT || 3006;
 
-app.listen(port, () => {
-    console.info(`Listing on port ${port}`);
-});
+app.listen(port, () => console.info(`Listing on port ${port}`));
